783cf9b1756f5631bea12c28612e2520a1911e6d
Rakha Wibisana <rakhaw@gmail.com>
2020-01-12 15:51:28 -0500

    program page get data

--- src/pages/our-program.js
+++ src/pages/our-program.js
@@ -3,11 +3,34 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const OurProgramPage = () => (
+const OurProgramPage = ({data}) => (
   <Layout>
     <SEO title="Our Program" />
     Our program
+    {console.log(data)}
   </Layout>
 )
+
+export const query = graphql`
+query {
+allMarkdownRemark {
+    edges {
+    node {
+        id
+        fields {
+        slug
+        }
+        frontmatter {
+        title
+        description
+        }
+        internal {
+        content
+        }
+    }
+    }
+}
+}  
+`
 
 export default OurProgramPage
